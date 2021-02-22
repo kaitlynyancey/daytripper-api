3f8c6e5677aa77d3033d33fe3f9fae67f6f07d4b
Kaitlyn Yancey <kyancey423@gmail.com>
2021-02-21 20:32:57 -0500

    updating test cases

--- src/app.js
+++ src/app.js
@@ -20,16 +20,16 @@ app.use(cors({
     origin: CLIENT_ORIGIN
 }))
 
-// app.use(function validateBearerToken(req, res, next) {
-//     const apiToken = process.env.API_TOKEN
-//     const authToken = req.get('Authorization')
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
 
-//     if (!authToken || authToken.split(' ')[1] !== apiToken) {
-//         logger.error(`Unauthorized request to path: ${req.path}`)
-//         return res.status(401).json({ error: 'Unauthorized request' })
-//     }
-//     next()
-// })
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        logger.error(`Unauthorized request to path: ${req.path}`)
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    next()
+})
 
 app.use('/api/trips', tripsRouter)
 
---
+++ test/trips.fixtures.js
@@ -0,0 +1,36 @@
+function makeTripsArray() {
+    return [
+        {
+            id: 1,
+            name: 'trip 1',
+            location: 'loc 1',
+            notes: 'test notes',
+            rating: 1
+        },
+        {
+            id: 2,
+            name: 'trip 2',
+            location: 'loc 2',
+            notes: 'test notes',
+            rating: 2
+        },
+        {
+            id: 3,
+            name: 'trip 3',
+            location: 'loc 3',
+            notes: 'test notes',
+            rating: 3
+        },
+        {
+            id: 4,
+            name: 'trip 4',
+            location: 'loc 4',
+            notes: 'test notes',
+            rating: 4
+        },
+    ];
+}
+
+module.exports = {
+    makeTripsArray,
+}
